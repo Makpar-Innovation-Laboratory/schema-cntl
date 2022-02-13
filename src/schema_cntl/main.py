9064e34713099be0316a68713d6059f7da6f3403
Grant Moore <gmoore@makpar.com>
2022-02-13 11:21:45 -0500

    stubs for diff functions

--- src/schema_cntl/main.py
+++ src/schema_cntl/main.py
@@ -4,42 +4,39 @@ import sys
 from argparse import ArgumentParser
 from pprint import pprint
 
-from innoldb.static.objects import StrutEncoder
-
 APP_DIR= os.path.dirname(os.path.abspath(__file__))
 SRC_DIR = os.path.dirname(APP_DIR)
 sys.path.append(SRC_DIR)
 
-from schema_cntl import settings
-from schema_cntl.schema import commit_schema, generate_revision_history
+from schema_cntl import settings, schema
+from schema_cntl.schema import commit_schema, generate_revision_history, generate_differences
 from schema_cntl.util.logger import getLogger
 
 log = getLogger('schema_cntl.main')
 
 def parse_cli_args(args):
     parser = ArgumentParser()
-    parser.add_argument('action', nargs='*', help = "Actions: commit, history")
+    parser.add_argument('action', nargs='*', help = "Actions: commit, history, diff")
     parser.add_argument('-str', '--start', help="Starting strand of the revision history", default=0, type=int)
-    parser.add_argument('-end', '--end', help="Ending strand of the revision history", default=1, type=int)
+    parser.add_argument('-no', '--number', help="Ending strand of the revision history", default=1, type=int)
     return parser.parse_args(args)
 
-def print_revision(index, start = 0):
-  if settings.LOG_LEVEL in ['INFO', 'DEBUG']:
-      index += start
-      print('---------------------------------------------- REVISION ', index)
+def print_revision_line(index, start = 0):
+    if settings.LOG_LEVEL in ['INFO', 'DEBUG']:
+        index += start
+        print('---------------------------------------------- REVISION ', index)
 
 def load_schema(file):
-  file_path = os.path.join(os.getcwd(), file)
+    file_path = os.path.join(os.getcwd(), file)
 
-  if os.path.exists(file_path):
-      with open(file_path, 'r') as infile:
-          schema = json.load(infile)
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as infile:
+            schema = json.load(infile)
 
-      return schema
+        return schema
 
-  else:
-      log.warning("No schema found at %s", file_path)
-      return None
+    log.warning("No schema found at %s", file_path)
+    return None
 
 def save_schema(file, schema):
     file_path = os.path.join(os.getcwd(), file)
@@ -60,25 +57,34 @@ def commit(file):
 
         log.info("Schema commited to DOCUMENT(meta_id=%s)", doc.meta_id)
 
-def history(file, start, end):
-    file_path = os.path.join(os.getcwd(), file)
+def history(file, start, no):
+    schema = load_schema(file)
 
-    if os.path.exists(file_path):
-        with open(file_path, 'r') as infile:
-            schema = json.load(infile)
+    if schema is not None:
 
         if schema.get('id', None) is None:
-            log.warning('Schema has not meta_id, please commit before generating history')
+            log.warning('Schema has no id, please commit before generating history')
             return
 
-        items = generate_revision_history(id=schema['id'], start=start, end=end)
+        items = generate_revision_history(id=schema['id'], start=start, no=no)
 
         for i, item in enumerate(items):
-            print_revision(i, start)
+            print_revision_line(i, start)
             pprint(item.schema.to_json())
+        
 
-    else:
-        log.warning("No schema found at %s", file_path)
+def diff(file, start, end):
+    schema = load_schema(file)
+
+    if schema is not None:
+
+        if schema.get('id', None) is None:
+            log.warning('Schema has no id, please commit before generating revision diff')
+            return
+
+        generate_differences(id=schema['id'], strand_start_index=start, strand_end_index=end)
+        
+
 
 def do_program(args):
     args = parse_cli_args(args)
@@ -87,10 +93,20 @@ def do_program(args):
         if len(args.action) > 1:
             commit(args.action[1])
         else:
-            log.warning("No file path inputted with commit action")
+            log.warning("Command is of the form `commit <path-to-schema.json>`")
+            return
     
     if args.action[0] == 'history':
-        history(args.action[1], args.start, args.end)
+        history(args.action[1], args.start, args.number)
+        return
+
+    if args.action[0] == 'diff':
+        if len(args.action[1:]) == 3:
+            diff(file=args.action[1], start=int(args.action[2]), end=int(args.action[3]))
+            pass
+        else:
+            log.warning("Command is of the form `diff <revision 1> <revision 2>`")
+            return 
 
 def entrypoint():
     """Entrypoint for build package
--- src/schema_cntl/schema.py
+++ src/schema_cntl/schema.py
@@ -24,13 +24,26 @@ def commit_schema(schema):
     schema_doc.save()
     return schema_doc
 
-def generate_revision_history(id, start = 0, end = 1):
+def generate_revision_history(id, start = 0, no = 1):
     schema_doc = Document(table=settings.TABLE, ledger=settings.LEDGER, id=id, stranded=True)
+    if start + no > len(schema_doc.strands):
+        raise KeyError("Too many strands specified")
+    return schema_doc.strands[start:start+no]
+
+def no_of_revisions(id):
+    return len(Document(table=settings.TABLE, ledger=settings.LEDGER, id=id, stranded=True).strands)
+
+def generate_differences(id, strand_start_index, strand_end_index):
+    schema_doc = Document(table=settings.TABLE, ledger=settings.LEDGER, id=id, stranded=True)
+
+    if strand_start_index > len(schema_doc.strands) - 1 or \
+      strand_end_index > len(schema_doc.strands):
+        raise ValueError("Specified indices exceed number of strands")
+
+    strand_start = schema_doc.strands[strand_start_index]
+    strand_end = schema_doc.strands[strand_end_index]
     
-    if start is not None and end is not None:
-        return schema_doc.strands[start:end]
-    elif start is not None and end is None:
-        return schema_doc.strands[start:]
-    elif start is None and end is not None:
-        return schema_doc.strands[:end]
-    return schema_doc.strands
+    print(strand_start.schema.tables[0]['name'])
+    print(strand_start.schema.tables[0]['columns'])
+    print(strand_end.schema.tables[0]['name'])
+    print(strand_end.schema.tables[0]['columns'])
