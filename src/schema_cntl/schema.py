ffd5b1dc3faab8ac9596384ec7aee28f9a487f7e
Grant Moore <gmoore@makpar.com>
2022-02-16 10:55:29 -0500

    build scripts

--- src/schema_cntl/schema.py
+++ src/schema_cntl/schema.py
@@ -1,20 +1,3 @@
-
-# 1. get first_revision
-#     a. create table statement
-#
-# 1. get revisions
-#     previous_revision = first_revision
-#
-#     for revision in revisions:
-#       
-#       a. find keys in revision not in previous_revision
-#           i. alter table statement for keys
-#
-#       b. find keys with values different from prevision
-#           i. alter column statements for key, values
-#
-#       c. previous_revision = revision
-
 from innoldb.qldb import Document
 
 from schema_cntl import settings
